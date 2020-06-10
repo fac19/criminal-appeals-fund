90a8a3951b91710a53a023afd460c9ef037a47bf
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-06-10 12:37:57 +0100

    Add content to application card component for profile page Relates #9 Relates #15
    Co-authored-by: lizzy-j <lizzyhj@hotmail.com>

---
+++ src/components/ApplicationCard/ApplicationCard.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: "100%",
+	},
+	backButton: {
+		marginRight: theme.spacing(1),
+	},
+	instructions: {
+		marginTop: theme.spacing(1),
+		marginBottom: theme.spacing(1),
+	},
+}));
+
+const ApplicationCard = () => {
+	const classes = useStyles();
+	const [activeStep, setActiveStep] = React.useState(0);
+	const steps = ["Stage 1", "Stage 2", "Stage 3", "Stage 4"];
+
+	const handleNext = () => {
+		setActiveStep((prevActiveStep) => prevActiveStep + 1);
+	};
+
+	const handleBack = () => {
+		setActiveStep((prevActiveStep) => prevActiveStep - 1);
+	};
+
+	return (
+		<div className={classes.root}>
+			<Stepper activeStep={activeStep} alternativeLabel>
+				{steps.map((label) => (
+					<Step key={label}>
+						<StepLabel>{label}</StepLabel>
+					</Step>
+				))}
+			</Stepper>
+			<ul>
+				<li>Stage 1: Your application satisfies the criteria</li>
+				<li>Stage 2: Your application has been selected for funding</li>
+				<li>
+					Stage 3: You will need to generate an invoice to receive your funding
+				</li>
+				<li>Stage 4: Your application is awaiting funding</li>
+			</ul>
+			<Button
+				disabled={activeStep === 0}
+				onClick={handleBack}
+				className={classes.backButton}>
+				Back
+			</Button>
+			<Button variant="contained" color="primary" onClick={handleNext}>
+				{activeStep === steps.length - 1 ? "Finish" : "Next"}
+			</Button>
+			{activeStep === 2 && (
+				<Button variant="contained" color="secondary">
+					Generate Invoice
+				</Button>
+			)}
+			{activeStep === 4 && <h3>Funded</h3>}
+		</div>
+	);
+};
+
+export { ApplicationCard };
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavbarLoggedIn } from "../../components/Navbar/Navbar";
+import { ApplicationCard } from "../../components/ApplicationCard/ApplicationCard";
 import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
@@ -9,6 +10,7 @@ const ProfilePage = () => {
 			<NavbarLoggedIn />
 			<h1>Applicant's profile page</h1>
 			<h2>Larry Bird</h2>
+			<ApplicationCard />
 			<Button>
 				<Link to="/apply">Apply for funding</Link>
 			</Button>

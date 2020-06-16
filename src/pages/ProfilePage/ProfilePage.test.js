997ec0ccfcf387beeeeb6011f91f0640e78ea9e6
Ina Yoon <itsina96@gmail.com>
2020-06-16 14:51:58 +0100

    Add test in profile page

--- src/pages/ProfilePage/ProfilePage.test.js
+++ src/pages/ProfilePage/ProfilePage.test.js
@@ -1,14 +1,6 @@
-// import React from "react";
-// import { screen, render } from "@testing-library/react";
-// import ProfilePage from "./ProfilePage";
-
-// // describe("Test works", () => {
-// // 	it("2 plus 2 equals 2", () => {
-// // 		expect(4).toBe(4);
-// // 	});
-// // });
-
-// test("Renders profile page", () => {
-// 	render(<ProfilePage />);
-// 	screen.getByText("APPLY FOR FUNDING");
-// });
+import React from "react";
+describe("Test works", () => {
+	it("2 plus 2 equals 2", () => {
+		expect(4).toBe(4);
+	});
+});

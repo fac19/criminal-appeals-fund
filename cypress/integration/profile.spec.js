dca5840f1c65dc891eb21852d01e3128d3349df8
Lizzy-j <lizzyhj@hotmail.com>
2020-06-18 18:12:52 +0100

    profile test

--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -2,15 +2,16 @@
 describe("Users should be able to have an overview of their applications and the stages they are in", () => {
 	it("Displays the Apply for funding button", () => {
 		cy.visit("/profile");
-		cy.get("[data-cy=funding").contains("Apply for funding");
+		cy.get("[data-cy=funding]").contains("Apply for funding");
 		cy.url().should("include", "/profile");
 		cy.get("ApplicantName").contains();
 	});
 
 	it("Displays the user's name", () => {
-		cy.visit("/profile");
 		// cy.contains("profile").click();
 		cy.url().should("include", "/profile");
-		cy.get(["data-cy=applicant-name"]).contains();
+		cy.get(["data-cy=applicant-name"]).contains("Hello, ");
 	});
+
+	it("Displays application card", () => {});
 });

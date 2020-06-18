ab99d403207396cf2957a7300e42c1902520799d
Lizzy-j <lizzyhj@hotmail.com>
2020-06-18 17:24:13 +0100

    create first test for profile spec

--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -1,1 +1,8 @@
 //As a user, I want to be up to date on the stages of my application
+describe("Users should be able to have an overview of their applications and the stages they are in", () => {
+	it("Displays the user's name", () => {
+		cy.visit("/profile");
+		cy.contains("Sign Up").click();
+		cy.url().should("include", "/signup");
+	});
+});
